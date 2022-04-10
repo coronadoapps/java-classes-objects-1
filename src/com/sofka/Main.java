67b46fc52560174d83c93f6039cca33389afd03b
stiven c <iamalescoronado@gmail.com>
2022-04-09 23:21:31 -0500

    Instance of two java objects

--- src/com/sofka/Main.java
+++ src/com/sofka/Main.java
@@ -5,12 +5,13 @@ import java.util.Date;
 public class Main {
 
     public static void main(String[] args) {
-        Person person1;
+        Person person1, person2;
         person1 = new Person("Stiven", "Coronado", "Borja", 1.80f);
         System.out.println(person1.getFullName());
 
         Date date = new Date();
-        System.out.println(date);
+        person2 = new Person("Juan", "Coronado", "Borja", 1.50f, date);
+        System.out.println(person2.getName());
     }
 
 }
